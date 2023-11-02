ad7d0e528ccf126441327ec9e86e892cad4fb6a4
Dereckr <dereckromos@gmail.com>
2023-11-02 18:54:08 -0400

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please input your grade percentage: ");
+        string grade = Console.ReadLine();
+        int gradeN = int.Parse(grade);
+        string letterGrade = "no grade was inserted yet";
+
+
+
+        if (gradeN >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradeN >= 80)
+        {
+            letterGrade = "B";
+        }
+
+        else if (gradeN >= 70)
+        {
+            letterGrade = "C";
+        }
+
+        else if (gradeN >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        else if (gradeN < 60)
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter Grade is {letterGrade}");
+
+        if (gradeN >= 70)
+        {
+            Console.WriteLine("Congratulations");
+
+        }
+
+        else
+        {
+             Console.WriteLine("You can do it");
+        }
+
+
+
+
+
+        
+
+
+
     }
 }
