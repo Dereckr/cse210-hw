721648e5a9530198f014e80ba7a6df4e3eadbf38
Dereckr <dereckromos@gmail.com>
2023-11-02 21:36:43 -0400

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Diagnostics.Tracing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // string n = Console.ReadLine();
+        // int number = int.Parse(n);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,11);
+
+        int guess;
+
+        
+        do
+        {
+        Console.Write("What is your guess? ");
+        string g = Console. ReadLine();
+        guess = int.Parse(g);
+
+        if (guess < number)
+        { 
+            Console.WriteLine("Higuer");
+        }
+
+        else if (guess > number)
+        {
+            Console.WriteLine("Lower");
+        }
+            
+
+        } while (guess != number);
+
+        Console.WriteLine("You guessed it!");     
+
     }
 }
